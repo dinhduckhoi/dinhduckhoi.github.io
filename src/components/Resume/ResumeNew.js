c33d2266dd21c41754f7d82200c8fc4afeb979f5
dinhduckhoi <51469740+dinhduckhoi@users.noreply.github.com>
2025-02-02 14:18:34 +0100

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/CV Resume English.pdf";
+import pdf from "../../Assets/../Assets/CV_2025-02-02_Dinh_Duc Khoi.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
